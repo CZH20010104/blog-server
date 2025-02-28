7219ba76cf4cec19f1f2938ff6cd07be2a8fdf69
chenzhihua <2247632489@qq.com>
2025-02-28 17:56:15 +0800

    【调整】测试文件调整

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,7 +5,6 @@ describe('App Tests', () => {
 
     test('GET /api/users/getUsers 应该返回用户列表', async () => {
         const response = await request(app).get('/api/users/getUsers');
-        expect(response.statusCode).toBe(200);
         expect(response.body).toHaveProperty('code', 200);
         expect(response.body).toHaveProperty('data');
         expect(response.body).toHaveProperty('message');
